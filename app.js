40dbd8f36598bf2ceaa6cd5c471e791719713390
kausar2697 <17103126@iubat.edu>
2020-03-31 21:17:20 +0600

    Add a callback funciton

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ function print(name){
     console.log('HELLO'+' '+name);
 }
 print('kausar');
+
+function callback(a,b,callback){
+ callback(a,b);
+}
+
+function add(a,b){
+    console.log(a+b);
+}
+
+function sub(a,b){
+    console.log(a-b);
+}
+
+callback(30,10,sub);
