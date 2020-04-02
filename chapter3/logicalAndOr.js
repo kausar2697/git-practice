7e0a8b285cac522a4ee527965b466e069cf2865d
kausar2697 <17103126@iubat.edu>
2020-04-03 00:16:43 +0600

    upto chapter 3

---
+++ chapter3/logicalAndOr.js
@@ -0,0 +1,20 @@
+//short hand 
+
+name= 'kausar'
+
+fullname = name || 'abc';
+
+
+//for Boolean
+
+var isok =false
+
+/*
+if(isok){
+    console.log('everything is ok!');
+
+}
+
+*/
+
+isok && console.log('everthing is ok');
---
+++ chapter3/ternary.js
@@ -0,0 +1,16 @@
+var a =30
+var b=69
+
+result= b%2 ===0?'even' : 'odd'
+
+console.log(result)
+
+/*
+Done] exited with code=0 in 0.196 seconds
+
+[Running] node "c:\Users\kausa\Documents\git-practice\chapter3\ternary.js"
+odd
+
+[Done] exited with code=0 in 0.197 seconds
+*/
+
