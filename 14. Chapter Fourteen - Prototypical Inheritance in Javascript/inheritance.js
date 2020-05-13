3d78a85a0372811511a019d75e549070fd13494b
kausar2697 <17103126@iubat.edu>
2020-05-14 00:04:31 +0600

    chapter 14

--- 14. Chapter Fourteen - Prototypical Inheritance in Javascript/inheritance.js
+++ 14. Chapter Fourteen - Prototypical Inheritance in Javascript/inheritance.js
@@ -1,4 +1,5 @@
 //#################inheritance#####################################
+/*
 function Shape(){
 
 }
@@ -25,5 +26,39 @@ Square.prototype.draw = function(){    //prototype extend
 
 
 var s1= new Square(37)
+
+s1.common()        //i am common Method
+
+//*************************************************************** */
+
+//***************************** Super (For passing parameter to a parent class)************************** */
+
+function Shape(color){
+ this.color= color
+} 
+
+Shape.prototype.common=function(){
+        console.log('i am common Method')
+    
+}
+
+function Square(width,color){
+    Shape.call(this,color)
+    this.width=width
+
+}
+
+
+Square.prototype= Object.create(Shape.prototype) //prototype reset
+Square.prototype.constructor= Square  //constructor reset
+
+
+
+Square.prototype.draw = function(){    //prototype extend
+        console.log('drawing')
+}
+
+
+var s1= new Square(37,80)
 
 s1.common()        //i am common Method
